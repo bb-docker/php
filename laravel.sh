b25e95d8a42794e83479f4d4b227dba2b4cfd0c6
bananabb <samson8682@gmail.com>
2021-04-09 11:43:29 +0800

    ADD: laravel.sh

---
+++ laravel.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+DEF_NAME="example"
+DEF_PORT="8000"
+
+CYAN="\e[36m"
+GREY="\e[37m"
+RESET="\e[0m"
+
+_hint() { echo -e "${GREY}($1)${RESET}"; }
+_quest() { echo -e "${CYAN}?${RESET}"; }
+
+cd /var/www/html
+read -r -p "$(_quest) Project Name $(_hint $DEF_NAME) " _NAME
+read -r -p "$(_quest) Export port $(_hint $DEF_PORT) " _PORT
+
+[ -z "$_NAME" ] && _NAME=$DEF_NAME
+[ -z "$_PORT" ] && _PORT=$DEF_PORT
+
+## Use Composer build the project
+composer create-project --prefer-dist laravel/laravel $_NAME
+cd $_NAME
+composer install
+
+## Setup the Nginx
+echo "server {
+    listen $_PORT;
+    root /var/www/html/$_NAME/public;
+    index index.html index.htm index.nginx-debian.html index.php;
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php8.0-fpm.sock;
+    }
+}" > /etc/nginx/sites-available/$_NAME
+ln -s /etc/nginx/sites-available/$_NAME /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+
+## Restart Nginx
+/etc/init.d/nginx reload
